07d2d6eae39abe3dc3953050295f07a563c1671e
abdulrehmanqasimali31-cmyk <abdulrehmanqasimali31@gmail.com>
2026-02-19 13:23:15 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, send_file
+import yt_dlp
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form.get('url')
+    choice = request.form.get('choice') # 'video' ya 'audio'
+    
+    # Downloads folder saaf rakhne ke liye
+    if not os.path.exists('downloads'): os.makedirs('downloads')
+
+    ydl_opts = {
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
+    }
+
+    if choice == 'audio':
+        ydl_opts['format'] = 'bestaudio/best'
+        ydl_opts['postprocessors'] = [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    else:
+        ydl_opts['format'] = 'bestvideo+bestaudio/best'
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        file_path = ydl.prepare_filename(info)
+        if choice == 'audio':
+            file_path = file_path.rsplit('.', 1)[0] + '.mp3'
+            
+    return send_file(file_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
